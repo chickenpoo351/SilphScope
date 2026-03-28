e3b4dcd58f1cc00d1e9c8da970a44ef389d3a18b
chickenPoo <ivanemerald1209@gmail.com>
2026-03-28 16:34:06 -0400

    extract.js added not really hooked up yet though still have to add the lz77 stuff plus turning this into a png

---
+++ src/graphics/extract.js
@@ -0,0 +1,13 @@
+import { lz77Decompress } from "./lz77-decompress.js";
+// import { rom } from "I will add this variable once I figure out the UX..."
+
+export function extract(asset) { // since well I havent actually tested this... I have no idea if it works or not... but eh that time will come eventually...
+    const start = asset.offset;
+    if (rom[start] === 0x10) {
+        const slice = rom.slice(start);
+        const data = lz77Decompress(slice);
+        return { ...asset, data, compressed: true };
+    }
+    const slice = rom.slice(start, start + asset.size);
+    return { ...asset, data: slice, compressed: false };
+}
