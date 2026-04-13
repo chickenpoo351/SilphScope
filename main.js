bf4a06cd134a6a6ac798309cbdcee8e93b28a996
chickenPoo <ivanemerald1209@gmail.com>
2026-04-12 22:59:37 -0400

    it worked!

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 import { renderMon } from "./src/graphics/mons/render-mons.js";
 import { renderMonIcon } from "./src/graphics/mons/render-mon-icon.js";
 import { renderMonFoot } from "./src/graphics/mons/render-mon-foot.js";
-import { extract } from "./src/graphics/extract.js";
+import { extract } from "./src/graphics/extract.js"; // random message for a commit lol
 
 export { renderMon, renderMonIcon, renderMonFoot, extract };
 
