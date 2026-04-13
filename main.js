871d2bd5d03c1c90aefd6b1ccd6a6e195af7029d
chickenPoo <ivanemerald1209@gmail.com>
2026-04-12 22:16:30 -0400

    main.js import update :p

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-import { renderMon } from "./src/graphics/render-mons.js";
-import { renderMonIcon } from "./src/graphics/render-mon-icon.js";
-import { renderMonFoot } from "./src/graphics/render-mon-foot.js";
+import { renderMon } from "./src/graphics/mons/render-mons.js";
+import { renderMonIcon } from "./src/graphics/mons/render-mon-icon.js";
+import { renderMonFoot } from "./src/graphics/mons/render-mon-foot.js";
 import { extract } from "./src/graphics/extract.js";
 
 export { renderMon, renderMonIcon, renderMonFoot, extract };
