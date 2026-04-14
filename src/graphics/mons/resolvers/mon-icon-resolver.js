88c5102ce80f429b93696d42d67ef3891ac99ca9
chickenPoo <ivanemerald1209@gmail.com>
2026-04-14 17:22:02 -0400

    something rendered this time...

--- src/graphics/mons/resolvers/mon-icon-resolver.js
+++ src/graphics/mons/resolvers/mon-icon-resolver.js
@@ -1,6 +1,6 @@
 export function resolveMonIcon(mon, reader) {
     const table = reader.getTable("monIconTable");
-    const entryOffset = table + mon.index * 8;
+    const entryOffset = table + mon.index * 4;
     const ptr = reader.readPointer(entryOffset);
     return {
         offset: ptr,
