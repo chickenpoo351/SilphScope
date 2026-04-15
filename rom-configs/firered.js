42a70c6b569e2ad2bd69af130aa2c4859a484ed1
chickenPoo <ivanemerald1209@gmail.com>
2026-04-15 19:28:09 -0400

    item icon table found took like 5 hours of playing around in Ghidra but I got it eventually!

--- rom-configs/firered.js
+++ rom-configs/firered.js
@@ -15,5 +15,6 @@ export const firered = {
         trainerFrontPicPaletteTable: 0x239a1C,
         trainerBackPicTable: 0x239FA4,
         trainerBackPicPaletteTable: 0x239FD4,
+        itemIconTable: 0x09899C, // so this was very hard to find since it isn't labeled in the .map of ROMs... luckily Ghidra and the ROM decomps exist so that helped a ton... issue is I believe this contains both the palette and gfx in each listing (should probably double check pokefirered to confirm...) so it will be interesting to extract assets I suppose...
     } // if this works out... well hehe... :D
 }
