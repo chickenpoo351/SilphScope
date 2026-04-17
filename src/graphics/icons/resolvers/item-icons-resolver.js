02c82fecd15a2409159bd74fb98e6b7e143b3aa8
chickenPoo <ivanemerald1209@gmail.com>
2026-04-16 21:22:40 -0400

    more debugging...

--- src/graphics/icons/resolvers/item-icons-resolver.js
+++ src/graphics/icons/resolvers/item-icons-resolver.js
@@ -2,6 +2,10 @@ export function resolveItemIconObject(item, reader, itemName, gfxOrPal) {
     const table = reader.getTable("itemIconTable");
     const entrySize = 8; // why did I make this variable... hmmm... I really can't remember... eh it doesn't do any harm other than being kinda useless since we could just pass 8 as is...
     const entryOffset = table + item.index * entrySize;
+    const rawPtr = reader.readU32(entryOffset);
+    console.log(`${itemName} raw table value: 0x${rawPtr.toString(16)}`);
+    const fileOffset = rawPtr - 0x08000000;
+    console.log(`${itemName} file offset: 0x${fileOffset.toString(16)}`);
     const iconPtr = reader.readPointer(entryOffset);
     const palettePtr = reader.readPointer(entryOffset + 4);
     let finalPtr;
