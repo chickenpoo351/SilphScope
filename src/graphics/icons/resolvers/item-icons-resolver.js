33556a40a6bb3951e1fc380938d2e68c0cbbb3b2
chickenPoo <ivanemerald1209@gmail.com>
2026-04-17 19:16:59 -0400

    ha! it worked I knew my math was right :D

--- src/graphics/icons/resolvers/item-icons-resolver.js
+++ src/graphics/icons/resolvers/item-icons-resolver.js
@@ -2,16 +2,11 @@ export function resolveItemIconObject(item, reader, itemName, gfxOrPal) {
     const table = reader.getTable("itemIconTable");
     const entrySize = 8; // why did I make this variable... hmmm... I really can't remember... eh it doesn't do any harm other than being kinda useless since we could just pass 8 as is...
     const entryOffset = table + item.index * entrySize;
-    const rawPtr = reader.readU32(entryOffset);
-    console.log(`${itemName} raw table value: 0x${rawPtr.toString(16)}`);
-    const fileOffset = rawPtr - 0x08000000;
-    console.log(`${itemName} file offset: 0x${fileOffset.toString(16)}`);
     const iconPtr = reader.readPointer(entryOffset);
     const palettePtr = reader.readPointer(entryOffset + 4);
     let finalPtr;
     if (gfxOrPal === "gfx") {
         finalPtr = iconPtr;
-        console.log(`gfx ${itemName} asset offset should be 0x${finalPtr.toString(16)}`);
         return {
             name: `item_${itemName}_${gfxOrPal}`,
             offset: finalPtr,
@@ -19,7 +14,6 @@ export function resolveItemIconObject(item, reader, itemName, gfxOrPal) {
         }
     } else if (gfxOrPal === "pal") {
         finalPtr = palettePtr;
-        console.log(`pal ${itemName} asset offset should be 0x${finalPtr.toString(16)}`);
         return {
             name: `item_${itemName}_${gfxOrPal}`,
             offset: finalPtr,
