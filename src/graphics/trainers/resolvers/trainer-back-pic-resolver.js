e92fc602956d9e9fe73bce25b98e14e9f932e40c
chickenPoo <ivanemerald1209@gmail.com>
2026-04-18 16:45:00 -0400

    welp turns out the backPics need a size...

--- src/graphics/trainers/resolvers/trainer-back-pic-resolver.js
+++ src/graphics/trainers/resolvers/trainer-back-pic-resolver.js
@@ -2,8 +2,15 @@ export function resolveTrainerBackPic(trainer, reader, trainerName) {
     const table = reader.getTable("trainerBackPicTable");
     const entryOffset = table + trainer.index * 8;
     const ptr = reader.readPointer(entryOffset);
+    let size;
+    if (trainerName === "RED" || trainerName === "LEAF") {
+        size = 10240;
+    } else {
+        size = 8192;
+    }
     return {
         name: `trainer_${trainerName}_back_pic`,
         offset: ptr,
+        size,
     }
 }
