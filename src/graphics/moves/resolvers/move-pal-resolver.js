cc277e0f5017d8d50f494d5796786909f2d4a3ca
chickenPoo <ivanemerald1209@gmail.com>
2026-05-27 16:49:25 -0400

    move resolvers added now for everything else...

---
+++ src/graphics/moves/resolvers/move-pal-resolver.js
@@ -0,0 +1,10 @@
+export function resolveMovePal(move, reader, moveName) {
+    const table = reader.getTable("moveAnimPaletteTable");
+    const entryOffset = table + move.index * 8;
+    const ptr = reader.readOffset(entryOffset);
+    return {
+        name: `move_${moveName}_pal`,
+        offset: ptr,
+        size: 40 // maybe this will continue working? perhaps? please?
+    }
+}
---
+++ src/graphics/moves/resolvers/move-pic-resolver.js
@@ -0,0 +1,10 @@
+export function resolveMovePic(move, reader, moveName) {
+    const table = reader.getTable("moveAnimPicTable");
+    const entryOffset = table + move.index * 8;
+    const ptr = reader.readOffset(entryOffset);
+    return {
+        name: `move_${moveName}_pic`,
+        offset: ptr
+        // I don't think this needs a size... since I think most of these are all lz77 compressed... if not more JSON time :/
+    }
+}
