7291acb36d28a9eb9ae04c1e9930fcdacddb6f69
chickenPoo <ivanemerald1209@gmail.com>
2026-07-03 23:25:45 -0400

    commited the wrong thing this was what i forgot to commit... but oh well enjoy the blank file I just commited :o

--- src/render-worker.js
+++ src/render-worker.js
@@ -60,7 +60,9 @@ parentPort.on("message", async (task) => { // I don't really know what I am doin
     }
 
     try {
-        const result = await functionType(task.objectName, ...functionJSONData, reader, rom); // ok so that should handle the functions... if only we could pass cb's through workers but oh well :p
+        const result = await functionType(task.objectName, ...functionJSONData, reader, rom, {
+            ...task.options,
+        }); // ok so that should handle the functions... if only we could pass cb's through workers but oh well :p
         parentPort.postMessage({
             result
         })
